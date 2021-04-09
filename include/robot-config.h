d39934f21e9025bbfdefa8ce390fd23ab484e90f
Sanjhee Gupta <sanjheegupta@Sanjhees-MacBook-Pro.local>
2021-04-09 16:36:25 -0700

    entire folder

---
+++ include/robot-config.h
@@ -0,0 +1,20 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor LeftFront;
+extern motor RightFront;
+extern motor LeftBack;
+extern motor RightBack;
+extern motor Center;
+extern motor LeftLift;
+extern motor RightLift;
+extern controller Controller1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
